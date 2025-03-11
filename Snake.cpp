1a1d2966aff3bbe284074473d6d98294fdb28a8a
NeKon69 <nobodqwe@gmail.com>
2025-03-11 16:18:52 +0300

    made it actually work

--- Snake.cpp
+++ Snake.cpp
@@ -7,121 +7,151 @@
 #include <iostream>
 #include <vector_alias.hpp>
 
-class SnakeGame : public sf::Drawable, public sf::Transformable {
+class SnakeGame : public sf::Drawable {
 private:
+    struct SnakeSegment {
+        sf::Vector2f coords;
+        bool head = false;
+    };
 
-	struct snake{
-		sf::Vector2f coords;
-		bool head = false;
-		snake* next = nullptr;
-	};
-	unsigned int size = snake_data.get_size();
-	sf::Vector2f				  prev_coords;
-	raw::vector<snake>			   snake_data;
+    raw::vector<SnakeSegment> snakeData;
+    sf::Vector2f prevCoords;
+    sf::Vector2f foodCoords;
 
+    sf::Keyboard::Scancode prevMove = sf::Keyboard::Scancode::W;
+    sf::Keyboard::Scancode currMove = sf::Keyboard::Scancode::W;
 
-	sf::Vector2f food_coords;
+    std::random_device rd;
+    std::mt19937 gen;
+    std::uniform_int_distribution<> xDist;
+    std::uniform_int_distribution<> yDist;
 
-	sf::Keyboard::Scancode prev_move;
-	
-	std::random_device			   rd;
-	std::mt19937				  gen;
-	std::uniform_int_distribution<> x;
-	std::uniform_int_distribution<> y;
 public:
-	SnakeGame() : snake_data(1), gen(rd()), x(0, 799), y(0, 599) {
-		snake_data.reserve(96000);
-		snake_data[0].coords = sf::Vector2f(100, 100);
-		snake_data[0].head = true;
-	}
+    SnakeGame() : gen(rd()), xDist(0, 79), yDist(0, 59) {
+        snakeData.reserve(50000);
+        snakeData.push_back({ {100, 100}, true });
+        generateApple();
+    }
 
-	void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
-		states.transform *= getTransform();
-		for (auto& i : snake_data) {
-			sf::RectangleShape rect({ 10, 10 });
-			if (i.head)
-				rect.setFillColor(sf::Color::Red);
-			else
-				rect.setFillColor(sf::Color::Green);
-			rect.setPosition(i.coords);
-			target.draw(rect, states);
-		}
-		sf::CircleShape food_coords_(5);
-		food_coords_.setPosition(food_coords);
-		target.draw(food_coords_, states);
-	}
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override {
+        for (const auto& segment : snakeData) {
+            sf::RectangleShape rect({ 10, 10 });
+            rect.setFillColor(segment.head ? sf::Color::Red : sf::Color::Green);
+            rect.setPosition(segment.coords);
+            target.draw(rect);
+        }
 
-	void move(const sf::Keyboard::Scancode& button) {
-		prev_move = button;
-	}
-	void move(const sf::Event::KeyPressed& button) {
-		prev_move = button.scancode;
-	}
+        sf::CircleShape food(5);
+        food.setPosition(foodCoords);
+        target.draw(food);
+    }
 
-	void move() {
-		if (size == 1) prev_coords = snake_data[0].coords;
-		if (prev_move == sf::Keyboard::Scancode::A) {
-			if (prev_move != sf::Keyboard::Scancode::D)
-				snake_data[0].coords.x -= 10;
-		}
+    void move(const sf::Keyboard::Scancode& button) {
+        currMove = button;
+    }
+    void move(const sf::Event::KeyPressed& button) {
+        currMove = button.scancode;
+    }
 
-		if (prev_move == sf::Keyboard::Scancode::D) {
-			if (prev_move != sf::Keyboard::Scancode::A)
-				snake_data[0].coords.x += 10;
-		}
-		if (prev_move == sf::Keyboard::Scancode::W) {
-			if (prev_move != sf::Keyboard::Scancode::S)
-				snake_data[0].coords.y -= 10;
-		}
-		if (prev_move == sf::Keyboard::Scancode::S) {
-			if (prev_move != sf::Keyboard::Scancode::W)
-				snake_data[0].coords.y += 10;
-		}
-		for (int i = snake_data.get_size() - 1; i > 0; --i) {
-			snake_data[i].coords = snake_data[i - 1].coords;
-		}
-	}
 
-	void add_snake() {
-		sf::Vector2f lastCoords = snake_data[snake_data.get_size() - 1].coords;
-		sf::Vector2f secondLastCoords = size == 1 ? prev_coords : snake_data[snake_data.get_size() - 2].coords;
 
-		sf::Vector2f newCoords = lastCoords;
-		if (lastCoords.x == secondLastCoords.x) {
-			if (lastCoords.y > secondLastCoords.y)
-				newCoords.y += 10;
-			else
-				newCoords.y -= 10;
-		}
-		else if (lastCoords.y == secondLastCoords.y) {
-			if (lastCoords.x > secondLastCoords.x)
-				newCoords.x += 10;
-			else
-				newCoords.x -= 10;
-		}
-		snake snk;
-		snk.coords = newCoords;
-		snk.next = &(snake_data[snake_data.get_size() - 1]);
-		snake_data.push_back(snk);
-		++size;
-	}
+    void move() {
+        prevCoords = snakeData[0].coords;
+        if (currMove == sf::Keyboard::Scancode::A) {
+            if (prevMove != sf::Keyboard::Scancode::D)
+            { snakeData[0].coords.x -= 10; prevMove = currMove; }
+            else
+                snakeData[0].coords.x += 10;
+        }
 
-	void generate_apple() {
-		float x_ = x(gen);
-		float y_ = y(gen);
-		food_coords = { x_, y_ };
-	}
+        if (currMove == sf::Keyboard::Scancode::D) {
+            if (prevMove != sf::Keyboard::Scancode::A)
+            { snakeData[0].coords.x += 10; prevMove = currMove; }
+            else
+                snakeData[0].coords.x -= 10;
+        }
+        if (currMove == sf::Keyboard::Scancode::W) {
+            if (prevMove != sf::Keyboard::Scancode::S)
+            { snakeData[0].coords.y -= 10; prevMove = currMove; }
+            else
+                snakeData[0].coords.y += 10;
+        }
+        if (currMove == sf::Keyboard::Scancode::S) {
+            if (prevMove != sf::Keyboard::Scancode::W)
+            { snakeData[0].coords.y += 10; prevMove = currMove; }
+            else
+                snakeData[0].coords.y -= 10;
+        }
 
-	void update(sf::Time te) {
-		if (te.asMilliseconds() > 100) {
-			move();
-			if (snake_data[0].coords == food_coords) {
-				add_snake();
-				generate_apple();
-			}
-		}
+        for (int i = 1; i < snakeData.get_size(); ++i) {
+            if (i == 1)
+                snakeData[1].coords = prevCoords;
+            else
+                snakeData[i].coords = snakeData[i + 1].coords;
+        }
 
-	}
+        prevCoords = snakeData[0].coords;
+        std::cout << "------------------COORDS------------------" << "\n";
+        for (int i = 0; i < snakeData.get_size(); ++i) {
+            std::cout << i + 1 << ": " << snakeData[i].coords.x << "    " << snakeData[i].coords.y << std::endl;
+        }
+        std::cout << "prev_coords" << ": " << prevCoords.x << "    " << prevCoords.y << "\n";
+    }
+
+    void add_snake() {
+        sf::Vector2f lastCoords = snakeData.get_size() == 1 ? prevCoords : snakeData[snakeData.get_size() - 1].coords;
+        sf::Vector2f secondLastCoords = snakeData[snakeData.get_size() - 2].coords;
+        sf::Vector2f newCoords = lastCoords;
+
+        SnakeSegment snk;
+        snk.coords = newCoords;
+        snakeData.push_back(snk);
+    }
+
+    void generateApple() {
+        bool valid = false;
+        while (!valid) {
+            foodCoords = { float(xDist(gen) * 10), float(yDist(gen) * 10) };
+            valid = true;
+            for (const auto& segment : snakeData) {
+                if (segment.coords == foodCoords) {
+                    valid = false;
+                    break;
+                }
+            }
+        }
+    }
+
+    void update(sf::Time deltaTime) {
+        static sf::Time elapsedTime;
+        elapsedTime += deltaTime;
+
+        if (elapsedTime.asMilliseconds() > 100) {
+            if(checkCollision())
+				std::cout << "Game over!" << std::endl;
+            move();
+            if (snakeData[0].coords == foodCoords) {
+                add_snake();
+                generateApple();
+            }
+            elapsedTime = sf::Time::Zero;
+        }
+    }
+
+    bool checkCollision() {
+        if (snakeData[0].coords.x < 0 || snakeData[0].coords.x >= 800 ||
+            snakeData[0].coords.y < 0 || snakeData[0].coords.y >= 600) {
+            return true;
+        }
+
+        for (size_t i = 1; i < snakeData.get_size(); ++i) {
+            if (snakeData[0].coords == snakeData[i].coords) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 };
 
 int main() {
